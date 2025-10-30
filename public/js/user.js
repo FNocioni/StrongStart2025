172175210277848e32cb79578d153f9cae36e5a3
AmberGraph41161 <73851833+AmberGraph41161@users.noreply.github.com>
2025-10-30 15:22:53 -0500

    userPage (#20)
    * added primitive spendings dashboard to userpage
    
    * user transactions should display now

--- public/js/user.js
+++ public/js/user.js
@@ -1,5 +1,28 @@
 var baseUrl = "http://localhost:5000/"
 
+var username = `${localStorage.getItem("user")}`;
 var user = document.getElementById("user");
+user.textContent = `Logged in as ${username}`;
 
-user.textContent = `Logged in as ${localStorage.getItem("user")}`;
+var spendingsInfoParagraph = document.getElementById("spendingsInfoParagraph");
+
+async function getTransactions() {
+	const params = {
+		'username': username
+	}
+
+	const response = await fetch(baseUrl + "transactions", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(params)
+	});
+
+	const responseData = await response.json();
+	spendingsInforParagraph.innterHTML = ""
+	for(let x = 0; x < responseData.data.length; x++) {
+		spendingsInforParagraph.innterHTML += `${responseData.data[x]} \n<br>\n<br>`
+		//console.log(responseData.data[x]);
+	}
+}
+
+getTransactions();
